1b38d260607348a7e022a83410825a3a06179ba4
Bastard <bamboo03-nv@mail.ru>
2022-06-05 10:28:48 +0500

    Added function to create a one file with more than 1 string

--- src/ru/bastard/Main.java
+++ src/ru/bastard/Main.java
@@ -1,15 +1,20 @@
 package ru.bastard;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    private static int length;
+    private static String dir;
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What do you want to do?: ");
         System.out.println("1. Just create random string");
-        System.out.println("2. Create a file with random string in it");
+        System.out.println("2. Create a files with random string in it");
+        System.out.println("3. Create a file with random strings in it");
         int answer = scanner.nextInt();
         switch (answer) {
             case 1:
@@ -18,14 +23,23 @@ public class Main {
                 break;
             case 2:
                 System.out.println("Input a length of string: ");
-                int length = scanner.nextInt();
+                length = scanner.nextInt();
                 System.out.println("Input a numbers of file: ");
                 int files = scanner.nextInt();
                 System.out.println("Input a full directory: ");
-                String dir = scanner.next();
+                dir = scanner.next();
                 dir = Files.directoryCorrector(dir);
-                StringGenerator.generateFile(length, files, dir);
+                StringGenerator.generateFiles(length, files, dir);
                 break;
+            case 3:
+                System.out.println("Input a length of strings: ");
+                length = scanner.nextInt();
+                System.out.println("Input a amount of strings: ");
+                int strings = scanner.nextInt();
+                System.out.println("Input a directory: ");
+                dir = scanner.next();
+                dir = Files.directoryCorrector(dir);
+                StringGenerator.generateFile(length, strings, dir);
         }
     }
 
--- src/ru/bastard/StringGenerator.java
+++ src/ru/bastard/StringGenerator.java
@@ -1,11 +1,12 @@
 package ru.bastard;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class StringGenerator {
 
@@ -22,7 +23,7 @@ public class StringGenerator {
         return generated_string;
     }
 
-    public static void generateFile(int iterations, int files, String directory) throws IOException {
+    public static void generateFiles(int iterations, int files, String directory) throws IOException {
         int iterator = 0;
         while(iterator < files){
             Path path = Paths.get(directory + "\\" + iterator + ".txt");
@@ -30,6 +31,18 @@ public class StringGenerator {
             Path writtenFilePath = Files.write(path, bs);
             iterator++;
         }
+    }
+
+    public static void generateFile(int iterations, int strings, String directory) throws IOException {
+        int iterator = 0;
+        ArrayList<String> list = new ArrayList<>();
+        Path path = Paths.get(directory + "\\rs.txt");
+        while(iterator < strings) {
+            byte[] bs = generate(iterations).getBytes(StandardCharsets.UTF_8);
+            list.add(new String(bs));
+            iterator++;
+        }
+        Files.write(path, list);
     }
 
 }
